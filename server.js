726f30b6523e7b247f7b117bad0c72330a305c69
unjal <unjal29@outlook.com>
2026-08-29 16:58:01 +0800

    feat: add Material Design navigation rail demo with branding assets and custom theme configuration

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const MIME_TYPES = {
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
   '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
   '.woff2': 'font/woff2',
   '.woff': 'font/woff',
   '.ttf': 'font/ttf'
